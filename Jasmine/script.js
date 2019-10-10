2fc7be0305ed8182bc99bc3f2f4adaa4113d8543
Jasmine Wagner <jasminewagner@hotmail.com>
2019-10-10 09:53:55 +0200

    update

---
+++ Jasmine/script.js
@@ -0,0 +1,17 @@
+
+
+var car = new Object();
+
+var car = {
+	name: "", 
+	model: "", 
+	makeYear, 
+	color, 
+	fuelType
+	
+	
+        firstName: "Jenny",
+        lastName: "Doe",
+        age: 30,
+}
+
